add4488c163785e911ddcba0058d1a330e1aef52
Haganeken <ktat73@csu.fullerton.edu>
2020-03-02 13:27:31 -0800

    updated db

--- image_upload.sql
+++ image_upload.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Feb 22, 2020 at 07:36 AM
+-- Generation Time: Mar 02, 2020 at 09:25 PM
 -- Server version: 5.7.26
 -- PHP Version: 7.2.18
 
@@ -30,13 +30,33 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `images`;
 CREATE TABLE IF NOT EXISTS `images` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `image` varchar(100) NOT NULL,
   `name` text NOT NULL,
   `date` date NOT NULL,
   `photographer` text NOT NULL,
-  `location` text NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `location` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `images`
+--
+
+INSERT INTO `images` (`id`, `image`, `name`, `date`, `photographer`, `location`) VALUES
+(10, '123123123', 'bob', '2020-03-03', 'test photog', 'someplace test'),
+(11, '12312312312', 'test2', '2020-03-06', 'test2', 'testloc2'),
+(12, '9a636e1e365d9d834dbe5266ce877bd7.jpg', 'Propaine', '2020-04-10', 'bobby', 'alren'),
+(13, '9a636e1e365d9d834dbe5266ce877bd7.jpg', 'Propaine', '2020-04-10', 'bobby', 'alren'),
+(14, '9a636e1e365d9d834dbe5266ce877bd7.jpg', 'Propaine', '2020-04-10', 'bobby', 'alren'),
+(15, 'download.png', 'test', '2019-01-01', 'bobby', 'test'),
+(16, 'download.png', 'test', '2019-01-01', 'bobby', 'test'),
+(17, 'download.png', 'test', '2019-01-01', 'bobby', 'test'),
+(18, 'download.png', 'test', '2019-01-01', 'bobby', 'test'),
+(19, 'download.png', 'test', '2019-01-01', 'bobby', 'test'),
+(20, '3qewoy.jpg', 'meme', '2030-12-15', 'Future', 'nowhere'),
+(21, '3qewoy.jpg', 'meme', '2030-12-15', 'Future', 'nowhere'),
+(22, '3qewoy.jpg', 'meme', '2030-12-15', 'Future', 'nowhere');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
