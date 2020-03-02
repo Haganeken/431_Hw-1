3cc745995cee03cf73cda2ce456be2715a2f1bfc
Haganeken <ktat73@csu.fullerton.edu>
2020-03-02 09:27:43 -0800

    basic form add + conn test

--- gallery.php
+++ gallery.php
@@ -1,10 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Gallery</title>
+  <body>
+    <a href="index.html">Upload photo</a>
+    <h1>Photo gallery</h1>
+    <div align="center">
+      <table style="width: 100%; border: 0">
+        <tr>
+
+
+
 <?php
   //initialize db connection
   $db = mysqli_connect("localhost", "root", "", "image_upload");
 
+
+//check fann_get_total_connections
+if (!$db) {
+  die("Connection to db failed: " . mysqli_connect_error());
+}
+
   // Initialize message variable
   $msg = "";
-
+  $result = mysqli_query($db, "SELECT * FROM images");
+  //query, get all from db
+  //print image and data
+  while ($row = mysqli_fetch_array($result)) {
+    echo "<div id='img_div'>";
+      echo "<img src='uploads/".$row['image']."' >";
+      echo "<p>".$row['name']."</p>";
+      echo "<p>".$row['date']."</p>";
+      echo "<p>".$row['photographer']."</p>";
+      echo "<p>".$row['location']."</p>";
+    echo "</div>";
+  }
   //When upload button is clicked
   if (isset($_POST['upload'])) {
   	//image file name
@@ -19,6 +49,7 @@
   	$target = "uploads/".basename($image);
 
     //insert values into db
+    //image BLOB?
   	$sql = "INSERT INTO images (image, name, date, photographer, location) VALUES ('$image','$name', '$date','$photographer', '$location')";
 
 
@@ -30,18 +61,11 @@
   		$msg = "Failed to upload";
   	}
   }
-  $result = mysqli_query($db, "SELECT * FROM images");
-  //query, get all from db
-  //print image and data
-  while ($row = mysqli_fetch_array($result)) {
-    echo "<div id='img_div'>";
-      echo "<img src='uploads/".$row['image']."' >";
-      echo "<p>".$row['name']."</p>";
-      echo "<p>".$row['date']."</p>";
-      echo "<p>".$row['photographer']."</p>";
-      echo "<p>".$row['location']."</p>";
-    echo "</div>";
-  }
+
 
 
 ?>
+</tr>
+</div>
+</body>
+</html>
